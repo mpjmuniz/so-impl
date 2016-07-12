bf89a1d1f600e4cee26df91f1c97a04da00b8e06
Marcelo Muniz <marcelo.pjmuniz@gmail.com>
2016-07-12 09:05:19 -0300

    Configuração Pronta

--- Eclipse/src/ui/ControladorAbaConfiguracao.java
+++ Eclipse/src/ui/ControladorAbaConfiguracao.java
@@ -4,12 +4,15 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import controle.Configuracao;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class ControladorAbaConfiguracao {
 
+	private Configuracao conf;
+	
 	@FXML
 	GridPane base;
 	
@@ -35,6 +38,7 @@ public class ControladorAbaConfiguracao {
 	}
 	
 	public void initData(Configuracao conf) {
+		this.conf = conf;
 		tfQtdInicialPaginas.setText(Integer.toString(conf.getQuantidadeInicialPaginas()));
 		tfTamanhoMaximoProcesso.setText(Integer.toString(conf.getTamanhoMaximoProcesso()));
 		tfEndLogico.setText(Integer.toString(conf.getEnderecoLogico()));
@@ -42,8 +46,45 @@ public class ControladorAbaConfiguracao {
 		tfTamanhoMS.setText(Integer.toString(conf.getTamanhoTotalMS()));
 		tfTamanhoPagina.setText(Integer.toString(conf.getTamanhoPagina()));		
 		
+		tfQtdInicialPaginas.textProperty().addListener(this::qtdInicialMudou);
+		tfTamanhoMaximoProcesso.textProperty().addListener(this::tamanhoMaxMudou);
+		tfEndLogico.textProperty().addListener(this::endLogMudou);
+		tfTamanhoMP.textProperty().addListener(this::tamMPMudou);
+		tfTamanhoMS.textProperty().addListener(this::tamMSMudou);
+		tfTamanhoPagina.textProperty().addListener(this::tamPagMudou);	
 	}
 	
+	public void qtdInicialMudou(ObservableValue<? extends String> prop, 
+            String oldValue, 
+            String newValue) {
+		conf.setQuantidadeInicialPaginas(Integer.parseInt(newValue));
+		System.out.println(conf.getQuantidadeInicialPaginas());
+	}
 	
-
+	public void tamanhoMaxMudou(ObservableValue<? extends String> prop, 
+            String oldValue, 
+            String newValue) {
+		conf.setTamanhoMaximoProcesso(Integer.parseInt(newValue));
+	}
+	
+	public void endLogMudou(ObservableValue<? extends String> prop, 
+            String oldValue, 
+            String newValue) {
+		conf.setEnderecoLogico(Integer.parseInt(newValue));
+	}
+	public void tamMPMudou(ObservableValue<? extends String> prop, 
+            String oldValue, 
+            String newValue) {
+		conf.setTamanhoTotalMP(Integer.parseInt(newValue));
+	}
+	public void tamMSMudou(ObservableValue<? extends String> prop, 
+            String oldValue, 
+            String newValue) {
+		conf.setTamanhoTotalMS(Integer.parseInt(newValue));
+	}
+	public void tamPagMudou(ObservableValue<? extends String> prop, 
+            String oldValue, 
+            String newValue) {
+		conf.setTamanhoPagina(Integer.parseInt(newValue));
+	}
 }
