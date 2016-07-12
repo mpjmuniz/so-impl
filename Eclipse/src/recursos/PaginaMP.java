de7d2e798ab786e57f35b0952d2b7e22911dbbf9
Arthur Vieira Silva <Arthur Vieira Silva>
2016-07-12 15:21:34 -0300

    Remove ultima atualização das paginas não alocadas

--- Eclipse/src/recursos/PaginaMP.java
+++ Eclipse/src/recursos/PaginaMP.java
@@ -1,24 +1,27 @@
 package recursos;
 
+import java.util.Date;
+
 public class PaginaMP extends Pagina {
 
 	public PaginaMP(int endFisico) {
-		super(endFisico);
+		super(endFisico);		
 		super.presente = true;
 		super.modificado = false;
-		super.utilizado = true;
+		super.utilizado = false;
 	}
 	
 	public void desalocar(){
 		super.desalocar();
 		super.modificado = false;
+		super.ultimaUtilizacao = null;
 	}
 	
 	public String toString(){
 		return  "Processo: \t" + ((this.dono != null) ? this.dono.getId() : "x") + "\n"
 				+ "Mod? \t\t" + ((this.modificado) ? "v" : "x") + "\n"
 				+ "Util? \t\t" + ((this.utilizado) ? "v" : "x") + "\n"
-				+ "Ultima Util.: " + this.ultimaUtilizacao.toString().substring(10, 19) + "\n"
+				+ "Ultima Util.: \t" + ((this.ultimaUtilizacao != null)? this.ultimaUtilizacao.toString().substring(10, 19): 'x') + "\n"
 				+ "End. Fisico: " + Long.toString(endFisico) + "\n";
 	}
 }
